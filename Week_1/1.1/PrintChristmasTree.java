3a2e9fe9677004c5e6fa3893d0624e9369f199ea
MKRadius <caomanhkhai110@gmail.com>
2023-10-24 15:45:49 +0300

    Week_1

---
+++ Week_1/1.1/PrintChristmasTree.java
@@ -0,0 +1,18 @@
+public class PrintChristmasTree {
+    public static void main(String[] args) {
+
+        int height = 4;
+        
+        for (int i = 0; i <= (height - 1); i++) {
+            for (int j = (height - 1); j >= i ; j--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < (2*i + 1); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println("");
+        }
+    }
+}
