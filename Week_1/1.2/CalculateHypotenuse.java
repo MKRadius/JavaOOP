1c0cdc0685e2b55312e3c39ba87a219f06bca89a
MKRadius <caomanhkhai110@gmail.com>
2023-10-29 12:14:21 +0200

    Week_1

--- Week_1/1.2/CalculateHypotenuse.java
+++ Week_1/1.2/CalculateHypotenuse.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CalcHypotenuse {
+public class CalculateHypotenuse {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
