88ecd49e7286f5bf51144c95a9d0dc7ecd8c5106
MKRadius <caomanhkhai110@gmail.com>
2023-12-16 21:13:20 +0200

    Week_7

---
+++ Week_7/7.2/currencyconverter/src/main/java/com/example/dao/CurrencyDAO.java
@@ -0,0 +1,75 @@
+package com.example.dao;
+
+import com.example.entity.Currency;
+import com.example.datasource.MariaDBConnection;
+import java.sql.*;
+import java.util.*;
+
+public class CurrencyDAO {
+    public List<Currency> getAllCurrencies() {
+        List<Currency> currencies = new ArrayList<>();
+        String query = "SELECT * FROM currency";
+
+        try {
+            Connection connection = MariaDBConnection.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                Currency currency = new Currency(
+                        resultSet.getInt("id"),
+                        resultSet.getString("abbreviation"),
+                        resultSet.getString("name"),
+                        resultSet.getDouble("rate"));
+                currencies.add(currency);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return currencies;
+    }
+
+    public String[] getAllAbbreviations() {
+        List<String> abbreviations = new ArrayList<>();
+        String query = "SELECT abbreviation FROM currency";
+
+        try {
+            Connection connection = MariaDBConnection.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                abbreviations.add(resultSet.getString("abbreviation"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return abbreviations.toArray(new String[0]);
+    }
+
+    public double getConversionRateByAbbreviation(String abbreviation) {
+        double conversionRate = 0.0;
+        String query = "SELECT rate FROM currency WHERE abbreviation = ?";
+    
+        try {
+            Connection connection = MariaDBConnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            
+            preparedStatement.setString(1, abbreviation);
+    
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    conversionRate = resultSet.getDouble("rate");
+                }
+            }
+    
+        } catch (SQLException e) {
+            e.printStackTrace(); // Handle the exception, log it, etc.
+        }
+    
+        return conversionRate;
+    }
+    
+}
